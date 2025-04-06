1e465060d8f05a8c1bba2764d5b106690c23b8f0
ewazna <e.m.wazna@gmail.com>
2025-04-06 13:08:55 +0200

    add empty state to Properties page

--- src/pages/realestates/update-properties-list.ts
+++ src/pages/realestates/update-properties-list.ts
@@ -12,8 +12,36 @@ export function updatePropertiesList(
     firstPropertyIndex,
     lastPropertyIndex,
   );
-  const figures = currentProperties.map((property) => createFigure(property));
-  list.replaceChildren(...figures);
+
+  if (properties.length === 0) {
+    list.replaceChildren(createEmptyState());
+  } else {
+    const figures = currentProperties.map((property) => createFigure(property));
+    list.replaceChildren(...figures);
+  }
+}
+
+function createEmptyState(): HTMLDivElement {
+  const container = document.createElement("div");
+  const emptyImage = document.createElement("img");
+  const emptyStateText = document.createElement("p");
+
+  container.style.height = "100%";
+  container.style.display = "flex";
+  container.style.flexDirection = "column";
+  container.style.justifyContent = "center";
+
+  emptyImage.setAttribute("src", "/icons/empty_state.svg");
+  emptyImage.style.width = "150px";
+  emptyImage.style.alignSelf = "center";
+
+  emptyStateText.innerHTML =
+    "No real estates found.<br/>Try adjusting your search criteria.";
+  emptyStateText.style.fontSize = "1.2rem";
+  emptyStateText.style.textAlign = "center";
+
+  container.append(emptyImage, emptyStateText);
+  return container;
 }
 
 export function clearPropertiesList(): void {
